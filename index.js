61cfd9edca6f62bfa2de30457981f5863d31397d
Claus Zinn <claus.zinn@uni-tuebingen.de>
2018-07-25 21:17:16 +0200

    Amended rest api (not 'get' prefix in service call); updated documentation, fixed bug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // 2018- Claus Zinn, University of Tuebingen
 // 
 // File: index.js (for rest-based service)
-// Time-stamp: <2018-07-25 09:32:19 (zinn)>
+// Time-stamp: <2018-07-25 12:45:34 (zinn)>
 // -------------------------------------------
 
 
@@ -33,6 +33,18 @@ const Matcher = require("./Matcher.js").default;
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+app.get('/doc/exampleJSON_page1.png', function (req, res) {
+    res.sendfile('./exampleJSON_page1.png');    
+})
+
+app.get('/doc/exampleJSON_page2.png', function (req, res) {
+    res.sendfile('./exampleJSON_page2.png');    
+})
+
+app.get('/doc/exampleResult.json', function (req, res) {
+    res.sendfile('./exampleResult.json');    
+})
+
 app.get('/', function (req, res) {
     res.sendfile('./index.html');
 })
@@ -41,19 +53,7 @@ app.get('/api', function (req, res) {
     res.sendfile('./index.html');    
 })
 
-/* for getAllTools, we allow same parameters than getApplicableTools, with default values as follows:
-
-   sortBy=tasks
-   includeToolsReqAuto=yes
-   includeWS=no
-
-*/
-
-app.get('/api/getAllTools',function(req,res){
-    res.send( new Matcher().findApplicableTools( req.query ) );
-});
-
-// localhost:4511/api/getTools?language=eng&includeWS=true&mimetype=application%2Fpdf&sortBy=tasks&includeToolsReqAuth=true
+// localhost:4511/api/tools?language=eng&includeWS=true&mimetype=application%2Fpdf&sortBy=tasks&includeToolsReqAuth=true
 
 /* parameters:
 
@@ -65,8 +65,8 @@ app.get('/api/getAllTools',function(req,res){
 
  */
 
-app.get('/api/getTools/', function(req, res) {
-    console.log('/api/getTools', req);    
+app.get('/api/tools/', function(req, res) {
+    console.log('/api/tools', req);    
     res.send( new Matcher().findApplicableTools( req.query ) );    
 });
 
@@ -77,17 +77,17 @@ console.log(`Running on http://${HOST}:${PORT}`);
 
 /* Example for path parameters (not supported)
 
-app.get('/api/getTools/:language-:mediatype',function(req,res){
+app.get('/api/tools/:language-:mediatype',function(req,res){
     const resourceDescription =
 	{
 	    language: {
 		threeLetterCode: req.params.language },
 	    mimetype: req.params.mediatype
 	};
-    console.log('getTools', resourceDescription);
+    console.log('tools', resourceDescription);
     const matcher = new Matcher();
     const applicableTools = matcher.findApplicableTools( resourceDescription, false);
-    console.log('getTools', applicableTools);
+    console.log('tools', applicableTools);
     res.send( applicableTools )
 });
 
